62eb005b76a6990e6964c871eca0997b0cde442f
jieyaxin <493657493@qq.com>
2017-12-07 11:09:14 +0800

    解亚鑫order提交第一次

---
+++ app/src/main/java/com/example/huanpet/view/order/order_fragment/FosterFragment.java
@@ -0,0 +1,32 @@
+package com.example.huanpet.view.order.order_fragment;
+
+import android.view.View;
+
+import com.example.huanpet.R;
+import com.example.huanpet.app.BaseFragment;
+
+/**
+ * Created by 解亚鑫 on 2017/12/7.
+ */
+
+public class FosterFragment extends BaseFragment{
+    @Override
+    public int getFragmetId() {
+        return R.layout.fragment_foster;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initAdapter() {
+
+    }
+}
