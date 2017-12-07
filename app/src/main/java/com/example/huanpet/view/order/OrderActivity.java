08745de0264a8fbcf28105691470f87f2b73f81c
jieyaxin <493657493@qq.com>
2017-12-07 11:26:00 +0800

    解亚鑫order提交第一次

--- app/src/main/java/com/example/huanpet/view/order/OrderActivity.java
+++ app/src/main/java/com/example/huanpet/view/order/OrderActivity.java
@@ -11,5 +11,6 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
     }
 }
