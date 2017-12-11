f67a7e70b8f79151860342a6ff1bcaf74e94d983
jieyaxin <493657493@qq.com>
2017-12-11 11:46:51 +0800

    解亚鑫order提交第一次

--- app/src/main/java/com/example/huanpet/view/set/SetActivity.java
+++ app/src/main/java/com/example/huanpet/view/set/SetActivity.java
@@ -1,8 +1,13 @@
 package com.example.huanpet.view.set;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -10,18 +15,17 @@ import com.example.huanpet.R;
 import com.example.huanpet.app.BaseActivity;
 import com.example.huanpet.utils.CustomTool;
 
-public class SetActivity extends BaseActivity {
+public class SetActivity extends BaseActivity implements View.OnClickListener {
 
     private CustomTool set_custom;
     private RelativeLayout jilu;
-    private RelativeLayout wife;
     private RelativeLayout clear;
     private RelativeLayout pingfen;
     private RelativeLayout guanyu;
     private TextView version;
     private RelativeLayout sycnc;
-
-
+    private TextView textView;
+    private CheckBox box;
 
     @Override
     public int initLayoutID() {
@@ -30,14 +34,16 @@ public class SetActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        textView = findViewById(R.id.textView1);
         set_custom = (CustomTool) findViewById(R.id.set_custom);
         jilu = (RelativeLayout) findViewById(R.id.jilu);
-        wife = (RelativeLayout) findViewById(R.id.wife);
         clear = (RelativeLayout) findViewById(R.id.clear);
         pingfen = (RelativeLayout) findViewById(R.id.pingfen);
         guanyu = (RelativeLayout) findViewById(R.id.guanyu);
         version = (TextView) findViewById(R.id.version);
         sycnc = (RelativeLayout) findViewById(R.id.sycnc);
+        box = findViewById(R.id.box);
+        box.setOnClickListener(this);
     }
 
     @Override
@@ -60,10 +66,33 @@ public class SetActivity extends BaseActivity {
                 finish();
             }
         });
+
     }
 
     @Override
     public void setMyAppTitle() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.box:
+
+                          Log.i("ischecked","111111111");
+                  box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                      @Override
+                      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                          Log.i("ischecked",isChecked+"");
+                          if (isChecked){
+                              box.setBackgroundResource(R.drawable.on);
+                          }else {
+                              box.setBackgroundResource(R.drawable.off);
+                          }
+                      }
+                  });
+                    break;
+            }
     }
 }
