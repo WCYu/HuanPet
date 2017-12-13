8c00ef753a0b7fb200cf709e5da05b6d9ff6af81
Administrator <1757181650@qq.com>
2017-12-13 10:02:46 +0800

    石星

---
+++ app/src/main/java/com/example/huanpet/view/pet/View/PickerItem.java
@@ -0,0 +1,14 @@
+package com.example.huanpet.view.pet.View;
+
+/**
+ * PickerView item接口类
+ */
+public interface PickerItem {
+
+	/**
+	 * 获取item要显示的文本
+	 * 
+	 * @return
+	 */
+    String getText();
+}
---
+++ app/src/main/java/com/example/huanpet/view/pet/adapter/MyAdapter.java
@@ -0,0 +1,39 @@
+package com.example.huanpet.view.pet.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017/12/8.
+ */
+
+public class MyAdapter extends FragmentPagerAdapter {
+
+    private ArrayList<String> titleList;
+    private ArrayList<Fragment> fragmentList;
+
+    public MyAdapter(FragmentManager fm, ArrayList<String> titleList, ArrayList<Fragment> fragmentList) {
+        super(fm);
+        this.titleList = titleList;
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titleList.get(position);
+    }
+}
+
