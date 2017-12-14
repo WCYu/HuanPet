a30a370d0306be900bb538d207ac6c1c1cc89c77
阿禹 <13466941275@163.com>
2017-12-14 08:50:07 +0800

    王超禹

--- app/src/main/java/com/example/huanpet/view/user/SignIn/SignInActivity.java
+++ app/src/main/java/com/example/huanpet/view/user/SignIn/SignInActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,7 +14,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.huanpet.R;
+import com.example.huanpet.app.AppService;
 import com.example.huanpet.app.BaseActivity;
+import com.example.huanpet.entity.RegisterBean;
+import com.example.huanpet.utils.OkhttpUtil;
+import com.example.huanpet.utils.PreferencesUtil;
+import com.example.huanpet.utils.util.CJSON;
+import com.example.huanpet.view.main.view.HomeActivity;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Response;
 
 public class SignInActivity extends BaseActivity implements View.OnClickListener {
 
@@ -45,6 +65,10 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
         login_qq = (TextView) findViewById(R.id.login_qq);
 
         header_titles.setOnClickListener(this);
+        btn_find_pwd.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
+        main_login_qq.setOnClickListener(this);
+        main_login_wachat.setOnClickListener(this);
     }
 
     @Override
@@ -53,6 +77,61 @@ public class SignInActivity extends BaseActivity implements View.OnClickListener
             case R.id.header_titles:
                 Intent intentheader = new Intent(SignInActivity.this,RegisterActivity.class);
                 startActivity(intentheader);
+                break;
+            case R.id.btn_login:
+                HashMap map=new HashMap();
+                map.put("userPhone",edit_phone.getText().toString());
+                map.put("password",edit_pwd.getText().toString());
+                FormBody.Builder builder = new FormBody.Builder();
+                String josn = CJSON.toJSONMap(map);
+                builder.add("data",josn);
+
+                OkhttpUtil.getInstance().postJson("http://123.56.150.230:8885/dog_family/" + "user/login.jhtml", builder.build(), new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        final String string = response.body().string();
+                        AppService.baseActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    JSONObject jsonObject=new JSONObject(string);
+                                   boolean ret = (boolean) jsonObject.get("ret");
+                                   if(ret){
+                                       JSONObject result = jsonObject.getJSONObject("result");
+                                       String userId = (String) result.get("userId");
+                                       PreferencesUtil instance = PreferencesUtil.getInstance();
+                                              instance .setUserId(userId);
+                                       Log.e("Tat------111111-------",userId);
+                                       Log.e("Tat------222222-------",instance.getUserId());
+                                       Intent in=new Intent(SignInActivity.this,HomeActivity.class);
+                                       startActivity(in);
+                                       finish();
+                                   }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+                    }
+                });
+                Toast.makeText(this, "登陆", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_find_pwd:
+                Toast.makeText(this, "忘记密码", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_login_qq:
+                Toast.makeText(this, "QQ", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.main_login_wachat:
+                Toast.makeText(this, "微信", Toast.LENGTH_SHORT).show();
+
                 break;
         }
     }
